0469b0712d13acb193078354c6e48239976295a1
Michael <AstroMike101@gmail.com>
2021-06-13 18:57:37 -0400

    reworded conditional statements, created final game function which runs the game using the previous functions

--- main.js
+++ main.js
@@ -9,52 +9,65 @@ function computerPlay() {
     
      
  }
- computerPlay();
+
 
  function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection) {
+        return "It's a tie!"
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats scissors!";
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock"){
+        return "You win! Paper beats rock!";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats paper!";
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lose! Paper beats rock!";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats Scissors!";
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beats Paper!";
+    }
+   
+
+    }
 
  
-//rock scenarios
-if (playerSelection = "rock" && computerSelection == "scissors") {
-return("You win! Rock beats scissors.");
-}
-
-else if (playerSelection = "rock" && computerSelection == "paper") {
-return("You lose! Paper beats rock.");
-}
-
-else if (playerSelection = "rock" && computerSelection == "rock") {
-return("It's a tie!");
-}
-
-// paper scenarios
-if (playerSelection = "paper" && computerSelection == "rock") {
-return("You win! Paper beats rock.");
-}
-
-else if (playerSelection = "paper" && computerSelection == "scissors") {
-return("You lose! Scissors beats paper.");
-}
-
-else if (playerSelection = "paper" && computerSelection == "paper") {
-return("It's a tie!");
-}
-//scissors
-if (playerSelection = "scissors" && computerSelection == "paper") {
-return("You win! Scissors beats Paper.");
-}
-else if (playerSelection = "scissors" && computerSelection == "rock") {
-return("You lose! Rock beats Scissors.");
-}
-else if (playerSelection = "scissors" && computerSelection == "scissors") {
-return("It's a tie!");
 
 
 
-}
-
-}
-
-playerSelection = window.prompt("Choose Rock, Paper, or Scissors!");
+ playerSelection = window.prompt("Choose Rock, Paper, or Scissors!");
 playerSelection = playerSelection.toLowerCase();
 computerSelection = computerPlay();
+
+
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+function game() {
+    for (i=1;i<=4; i++){
+    playerSelection = window.prompt("Choose Rock, Paper, or Scissors!");
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+
+    }
+
+}
+
+
+game();
+
+
+
+
+
+
+
