e79830f3f88bf23742025ce4968319695c9d3843
Michael <AstroMike101@gmail.com>
2021-06-16 17:20:52 -0400

    created logic for the reset button

--- main.js
+++ main.js
@@ -2,12 +2,24 @@
  let playerScore = 0;
  let computerScore = 0; 
 
+ 
 
 
 const playerScoreText = document.querySelector('.playerScoreText');
 const computerScoreText = document.querySelector('.computerScoreText');
 const resultText = document.querySelector('.resultText');
 
+const resetScore = document.querySelector('.resetScore');
+
+resetScore.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreText.textContent = 0;
+    computerScoreText.textContent = 0;
+    resultText.textContent = "Start new game!";
+    
+})
+    
 
 
 
@@ -71,51 +83,61 @@ scissors.addEventListener('click', () => {
     if (playerSelection == computerSelection) {
         
         resultText.textContent ="It's a tie!";
+        resultText.style.color = "#EA4C46";
+
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
-        
+        resultText.style.color = "black";
         playerScore++;
         playerScoreText.textContent = `${playerScore}`;
         resultText.textContent = "You win! Rock beats scissors!";
         
     }
     else if (playerSelection == "paper" && computerSelection == "rock"){
+        resultText.style.color = "black";
         playerScore++;
         playerScoreText.textContent = `${playerScore}`; 
         resultText.textContent = "You win! Paper beats rock!";
 
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
+        resultText.style.color = "black";
         playerScore++;
         playerScoreText.textContent = `${playerScore}`;
         resultText.textContent = "You win! Scissors beats paper!";
         
     }
     else if (playerSelection == "rock" && computerSelection == "paper") {
+        resultText.style.color = "black";
         computerScore++;
         computerScoreText.textContent = `${computerScore}`; 
         resultText.textContent ="You lose! Paper beats rock!";
         
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
+        resultText.style.color = "black";
         computerScore++;
         computerScoreText.textContent = `${computerScore}`; 
        resultText.textContent = "You lose! Rock beats Scissors!";
         
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
+        resultText.style.color = "black";
         computerScore++;
         computerScoreText.textContent = `${computerScore}`; 
         resultText.textContent ="You lose! Scissors beats Paper!";
         
     }
-    console.log(playerScore);
-    if (playerScore >= 5) {
-        
+    
+    if (playerScore >= 5) {  
+        resultText.style.color = '#173F5F';
         resultText.textContent = "You win the game! You're better then a computer!";
+        
+
     }
 
     else if (computerScore >=5 ) {
+        resultText.style.color = '#173F5F';
         resultText.textContent = "You lose the game! Computer wins! "
     }
     
@@ -127,8 +149,7 @@ scissors.addEventListener('click', () => {
 
 }
         
-    
-    
+
 
 
     
